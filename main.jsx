daa637ffad4ffb9a5280f113ccc58cc4f335efc3
hero4147 <choyu4147@gmail.com>
2025-05-26 13:44:23 +0900

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+function App() { return <h1>Cosmetic Compare App 🧴</h1>; }
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
